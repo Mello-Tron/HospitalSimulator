b12422a3d3618e2372cf9aaf251d9014344fe854
Patrick <patricktrherbert@gmail.com>
2015-05-08 15:09:00 -0700

    Updated Caregiver and CityMap

---
+++ HospitalSimulation/HospitalSimulation/Caregiver.h
@@ -0,0 +1,18 @@
+#ifndef _CAREGIVER_
+#define _CAREGIVER_
+
+class Caregiver() {
+private:
+	bool available;
+	Patient currentPatient;
+public:
+	bool IsAvailable() {
+
+	}
+
+	void Work(int clock) {
+
+	}
+};
+
+#endif _CAREGIVER_
---
+++ HospitalSimulation/HospitalSimulation/CityMap.h
@@ -0,0 +1,15 @@
+#ifndef _CITYMAP_
+#define _CITYMAP_
+#include <string>
+
+class CityMap() {
+private:
+	Map<string, Patient> pMap;
+	vector<string> names;
+public:
+	void Update(int clock) {
+
+	}
+};
+
+#endif _CITYMAP_
