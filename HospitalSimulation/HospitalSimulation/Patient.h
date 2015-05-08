5194feb1ad8fce8291d9c73fc179d899255c3bcd
Michael <minimearnold@gmail.com>
2015-05-08 15:52:23 -0700

    Implementation
    Worked on Update() function for PatienQueue and worked on Patient

--- HospitalSimulation/HospitalSimulation/Patient.h
+++ HospitalSimulation/HospitalSimulation/Patient.h
@@ -1,12 +1,24 @@
 #ifndef _PATIENT_
 #define _PATIENT_
-
+#include <string>
+#include <iostream>
+using namespace std;
 
 class Patient {
 private:
-
+	int arrivalTime;
+	int treatmentTime;
+	int departureTime;
+	int averageWaitTime;
+	int priority;
+	int totalVisits;
+	string name;
 public:
+	Patient(int clock) {}
 
+	int getPriority() {
+		return priority;
+	}
 };
 
 #endif _PATIENT_
--- HospitalSimulation/HospitalSimulation/PatientQueue.h
+++ HospitalSimulation/HospitalSimulation/PatientQueue.h
@@ -1,12 +1,43 @@
 #ifndef _PATIENTQUEUE_
 #define _PATIENTQUEUE_
+#include <iostream>
+#include <queue>
+#include "Patient.h"
 
+using namespace std;
 class PatientQueue {
 
 private:
 
+	priority_queue<Patient> patientPriorityQueue;
+
 public:
 
+	void Update(int clock) {
+
+		Doctor->Work();
+		Nurse->Work();
+
+		bool newPatient;
+
+		while (newPatient = true) {
+			if (/*Nurse is available && Patient.getPriority > 0 && Patient.getPriority <= 10*/) {
+				//Assign patient to Nurse
+				//Restart WHILE loop
+			}
+			else if (/*Doctor is available && Patient.getPriority >= 11 && patient.getPriority <= 20*/) {
+				//Assign patient to Doctor
+				//Restart WHILE loop
+			}
+			else if (/*Doctor is available && Patient.getPriority >= 1 && patient.getPriority <= 10 */) {
+				//Assign patient to Doctor
+				//Restart WHILE loop
+			}
+			else
+				newPatient = false;
+		}
+		
+	}
 
 };
 #endif _PATIENTQUEUE_
