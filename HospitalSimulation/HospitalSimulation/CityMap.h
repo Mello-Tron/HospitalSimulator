f52464346f3530dcaf1a23950a4b3406d7ec022b
marnold22 <minimearnold@gmail.com>
2015-05-11 13:27:31 -0700

    Added readFile() function

--- HospitalSimulation/HospitalSimulation/CityMap.h
+++ HospitalSimulation/HospitalSimulation/CityMap.h
@@ -2,18 +2,36 @@
 #define _CITYMAP_
 
 #include <map>
+#include <utility>
 #include <vector>
 #include <string>
+#include <iostream>
+#include <fstream>
 #include "Patient.h"
+using namespace std;
 
 class CityMap {
 private:
 	std::queue<Patient*> done;  // might not be necessary, but putting it in just for right now
-	std::map<std::string, Patient> pMap;
+	std::map<std::string, Patient*> pMap;
 	std::vector<std::string> names;
 public:
 
+	void readFile(int clock) {
 
+		string line;
+		fstream citizens("C:\\Users\marnold17\Documents\GitHub\HospitalSimulator\HospitalSimulation\residents_273ville.txt");
+		if (!citizens) {
+			cout << "Can't open file!" << endl;
+		}
+		else {
+			while (citizens >> line) {
+				pMap.insert(make_pair(line, new Patient(clock)));
+				names.push_back(line);
+			}
+		}
+
+	}
 
 	void Update(int clock) {
 
